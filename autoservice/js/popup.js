53f08487a496461109e8f88945032a67ebd73316
Anastas <54882032+Anasteisa@users.noreply.github.com>
2019-11-07 13:07:41 +0300

    Add files via upload

---
+++ autoservice/js/popup.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() { // Ждём загрузки страницы
+	
+	$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
+		$(".popup").fadeOut(800);	// Медленно убираем всплывающее окно
+	});
+
+});
+
+
+$('.show_popup').click (function showPopup() {
+    $(".popup").fadeIn(800); // Медленно выводим изображение
+    })
